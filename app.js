7a683ef53b906202dacce43c8127f3fb1b53efff
kroman824 <karenroman824@gmail.com>
2024-06-05 16:05:02 -0400

    initial

---
+++ app.js
@@ -0,0 +1,26 @@
+//setting myAge variable to age
+const myAge = 23;
+
+//changing variable for earlyYears
+let earlyYears = 2;
+earlyYears *= 10.5;
+
+//Subtracting two years from myAge variable for it to equal in laterYears
+let laterYears = myAge - 2;
+
+//calculating the number of dog years
+laterYears *= 4;
+
+//checking work
+console.log(earlyYears);
+console.log(laterYears);
+
+//converting my age in dog years
+myAgeInDogYears = earlyYears + laterYears;
+
+//creating a string
+'Karen'.toLowerCase;
+const myName = 'Karen';
+
+//writing a statement
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} old in dog years.`);
